fa2fc6f00e83144d6ff092285ae734fcea142801
jongsang1492 <jongsangyoo2021@gmail.com>
2025-05-09 03:12:23 +0900

    Update ur10e_add_box.py

--- scripts/ur10e_add_box.py
+++ scripts/ur10e_add_box.py
@@ -5,6 +5,7 @@ import geometry_msgs.msg
 from moveit_commander.planning_scene_interface import PlanningSceneInterface
 import time
 
+# Initialize the ROS node and MoveIt commander
 rospy.init_node("ur10e_add_box_node", anonymous=True)
 moveit_commander.roscpp_initialize([])
 
@@ -12,12 +13,12 @@ scene = PlanningSceneInterface()
 robot = moveit_commander.RobotCommander()
 group = moveit_commander.MoveGroupCommander("manipulator")
 
-rospy.sleep(2)  # scene 초기화 기다림
+rospy.sleep(2)  # Wait for the planning scene to initialize
 
-# 박스 이름 정의
+# Define the box name
 box_name = "obstacle_box"
 
-# 박스 포즈 정의 (로봇 정면 0.35m 앞에 있는 장애물)
+# Define the pose of the box (an obstacle placed 0.35m in front of the robot)
 box_pose = geometry_msgs.msg.PoseStamped()
 box_pose.header.frame_id = robot.get_planning_frame()
 box_pose.pose.orientation.w = 1.0
@@ -25,8 +26,7 @@ box_pose.pose.position.x = 0.4
 box_pose.pose.position.y = -0.35
 box_pose.pose.position.z = 0.15
 
-# 박스 추가 (사이즈: 20cm x 20cm x 20cm)
+# Add the box to the planning scene (size: 20cm x 20cm x 20cm)
 scene.add_box(box_name, box_pose, size=(0.2, 0.2, 0.2))
 
-rospy.loginfo("박스 추가 완료!")
-
+rospy.loginfo("Box added to the planning scene!")
